71af36b61f09136cfa0bddba12f26a215effdd75
Mike Morris <michael.m.morris@pearson.com>
2019-06-04 18:05:36 -0500

    appease the linting gods

--- atomic-file.go
+++ atomic-file.go
@@ -2,10 +2,12 @@ package main
 
 import "os"
 
+// AtomicFile represents a file created with exclusive mode for use as a simple lock file
 type AtomicFile struct {
 	*os.File
 }
 
+// NewAtomicFile returns an AtomicFile at the specified path, which supports all methods of an os.File type
 func NewAtomicFile(path string) (*AtomicFile, error) {
 	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0600)
 	if err != nil {
